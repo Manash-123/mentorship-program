f3337b84edebba001fb06cb90176271c4c6da023
Manash-123 <gouda.manash1999@gmail.com>
2024-06-25 22:47:38 +0530

    HTML page created

---
+++ index.js
@@ -0,0 +1,10 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('Input field cannot be blank. Please write something');
+    } else {
+        
+    }
+}
