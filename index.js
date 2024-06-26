f6a446e41336c522ad67d036453bdacd10a1bc47
Manash-123 <gouda.manash1999@gmail.com>
2024-06-26 21:03:51 +0530

    WOrked on functionality

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ function addTask(){
     if(inputBox.value === ''){
         alert('Input field cannot be blank. Please write something');
     } else {
-        
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = 'X';
+        li.appendChild(span)
+        // console.log("3333333333333", listContainer)
     }
+    inputBox.value = '';
+    setData();
 }
+
+listContainer.addEventListener('click', function(e){
+    // console.log(e.target.tagName)
+    if(e.target.tagName === 'SPAN') {
+        // console.log('22222',e.target.parentElement)
+        e.target.parentElement.remove('li')
+    }
+    setData();
+})
+
+function setData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function getData(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+getData();
